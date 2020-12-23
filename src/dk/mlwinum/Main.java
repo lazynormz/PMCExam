b7ca6d25b2e7bcd2a85b2ab717d1d264fdea5408
lazynormz <skupi1234@gmail.com>
2020-12-23 22:28:53 +0100

    Basic FXML setup

---
+++ src/dk/mlwinum/Main.java
@@ -0,0 +1,22 @@
+package dk.mlwinum;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("view/MainView.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
